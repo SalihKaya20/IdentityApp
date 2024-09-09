33ae76c52935a3d9c8c1b0adbdfc4458a263074b
SalihKaya20 <123274161+SalihKaya20@users.noreply.github.com>
2024-09-09 19:39:03 +0300

    IdentityApp
    In this web application, which provides user and role management with ASP.NET Core Identity, e-mail confirmation is required for registration and users can reset their password via e-mail with the "Forgot My Password" option.
    There is a users and roles page and
    Only the admin user can see the roles and users section and change users' roles.

---
+++ IdentityApp/ViewModels/ResetPasswordModel.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels
+{
+    public class ResetPasswordModel
+    {
+
+        [Required]
+        public string Token { get; set; } =string.Empty;
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } =string.Empty;
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } =string.Empty;
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage ="Password do not match.")]
+        public string ConfirmPassword { get; set; } =string.Empty;
+
+    }
+}
